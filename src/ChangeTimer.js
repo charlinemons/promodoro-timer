21c3dca35de3ae24ecbbad81c352f6771f1d1d78
Charline Mons <charline.mons@gmail.com>
2024-04-08 14:45:26 -0400

    Added the css of buttons Timer

--- src/ChangeTimer.js
+++ src/ChangeTimer.js
@@ -1,12 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ChangeTimer.css";
 
 const ChangeTimer = ({ changeTimer }) => {
+  const [selectedButton, setSelectedButton] = useState(null);
+
+  const handleButtonClick = (timer) => {
+    changeTimer(timer);
+    setSelectedButton(timer);
+  };
+
   return (
     <div className="ChangeTimer">
-      <button onClick={() => changeTimer(300)}>Short break</button>
-      <button onClick={() => changeTimer(900)}>Long break</button>
-      <button onClick={() => changeTimer(1500)}>Focus</button>
+      <button
+        className={
+          selectedButton === 300
+            ? "changeTimerButton active"
+            : "changeTimerButton"
+        }
+        onClick={() => handleButtonClick(300)}
+      >
+        Short break
+      </button>
+      <button
+        className={
+          selectedButton === 900
+            ? "changeTimerButton active"
+            : "changeTimerButton"
+        }
+        onClick={() => handleButtonClick(900)}
+      >
+        Long break
+      </button>
+      <button
+        className={
+          selectedButton === 1500
+            ? "changeTimerButton active"
+            : "changeTimerButton"
+        }
+        onClick={() => handleButtonClick(1500)}
+      >
+        Focus
+      </button>
     </div>
   );
 };
